361cfd073365a3d23960a484bd2f059d2e6e1628
noje_ <noje_i@yahoo.com>
2021-10-07 13:09:13 +0300

    commit

---
+++ math class/src/MathClass.java
@@ -0,0 +1,48 @@
+
+//
+//public class MathClass
+//{
+//    public static void main(String[] args) {
+//
+//        double x = 3.14;
+//        double y = -10;
+//
+//        double z = Math.max(x,y);//si cuu min,
+//
+//        System.out.println(z);
+//
+//        System.out.println(Math.abs(y));
+//    }
+//}
+
+
+
+
+
+
+
+
+//Random
+
+import java.util.Random;
+
+public class MathClass
+{
+    public static void main(String[] args) {
+
+        Random rd = new Random();
+
+        //int x = rd.nextInt(6) + 1;
+        //System.out.println(x);
+
+//        double y = rd.nextDouble();
+//        System.out.println(y);
+
+
+          boolean z = rd.nextBoolean();
+        System.out.println( z);
+
+
+
+    }
+}
