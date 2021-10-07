f29723680ca3487f9b765daa2356ee1e43cdde7b
noje_ <noje_i@yahoo.com>
2021-10-07 12:33:52 +0300

    commit

--- variabile/src/MainClass.java
+++ variabile/src/MainClass.java
@@ -1,4 +1,22 @@
 public class MainClass
 {
+    public static void main(String[] args) {
 
+    }
 }
+
+
+
+
+
+//data type     size      primitive/reference           value
+
+//boolean       1 bit           primitive               true/false
+//byte          1 byte          primitive              -128 to 127
+//short         2 bytes         primitive               -32,768 to 32,767
+//int           4 bytes         primitive               -2 billion to 2 billion
+//long          8 bytes         primitive               -9 quintillion to 9 quintillion
+//float         4 bytes         primitive               fractionar numver up to 6-7 digits ex 3.141592
+//double        8 bytes         primitive               fractionar numver up to 15 digits ex 3.141592...
+//char          2 bytes         primitive               single character/letter/ASCII value
+//String        varies          reference               a sequence of characters ex Hello World
