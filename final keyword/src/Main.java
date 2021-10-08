08622e4a3146acead7c75ade7eed6a7e869f5f35
noje_ <noje_i@yahoo.com>
2021-10-08 14:27:21 +0300

    commit

---
+++ final keyword/src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args) {
+
+        //tot ce este declarat ca final nu mai poate fi schimbat pe parcursul codului
+
+        final double PI = 3.14159;
+
+
+        System.out.println(PI);
+
+
+    }
+}
