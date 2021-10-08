bb032114391808b802cf41c0b756db756213fa49
noje_ <noje_i@yahoo.com>
2021-10-08 15:16:45 +0300

    commit

---
+++ to string method/src/Car.java
@@ -0,0 +1,13 @@
+public class Car
+{
+    String make = "ferrari";
+    String model = "458";
+    int year = 2013;
+    String color = "red";
+
+    public String toString()
+    {
+        return make + model + color + year;
+    }
+
+}
