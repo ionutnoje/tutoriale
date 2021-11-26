b984c1b57f7a21ce005e8cc6128b1b2131c2e270
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2021-11-26 14:49:45 +0200

    commit

---
+++ java/Data Structures/Stack/src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args) {
+        //stack = LIFO data structure. Last-In First-Out
+        //          stores objects into a sort of "vertical tower"
+        //          push() to add to the top
+        //          pop() to remove from the top
+    }
+}
