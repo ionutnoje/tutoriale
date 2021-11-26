793b57b1a79e3a3bf651dff528028060222da65c
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2021-11-26 20:03:46 +0200

    LSI

---
+++ java/LSI/src/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+
+        LSI lsi = new LSI();
+
+        lsi.add(1);
+        lsi.add(2);
+        lsi.add(3);
+        lsi.toString();
+
+    }
+}
