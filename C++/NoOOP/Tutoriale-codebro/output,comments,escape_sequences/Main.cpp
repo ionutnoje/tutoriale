47bc4d1b2c7ddde81ea30ed8d5304989aed527b6
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2021-11-28 21:57:44 +0200

    commit la primul tutorial de la codebro pe c++

---
+++ C++/NoOOP/Tutoriale-codebro/output,comments,escape_sequences/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+    std::cout << "exemplu de mesaj\n";//daca folosim using namespace std nu mai este nevoie sa adaugam inainte de cout std::
+
+
+    cout << "mesajul asta o sa apara pe ecran \n"; //\n se foloseste ca sa treaca la urmatorul rand pe ecran
+    cout << "si mesajul asta o sa ajunga pe ecran " << endl;//endl este folosit ca si \n
+
+
+
+    return 0;//se pune la final de program mereu
+}
