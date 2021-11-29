e9a1f86839267f1f64c42ebab79cd73f14eeb8f9
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2021-11-29 19:41:25 +0200

    commit pointer to pointer

---
+++ C++/NoOOP/pointers(freeCodeCamp)/Pointers as function arguments - call by reference/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+/*
+    
+*/
+
+
+
+int main()
+{
+    //pointers as function arguments- call by reference
+
+
+
+
+
+    return 0;
+}
