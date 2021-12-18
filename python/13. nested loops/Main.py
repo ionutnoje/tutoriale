13d2f0d3c5ba292b078ab5aaa6dd69c488048e49
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2021-12-18 22:09:29 +0200

    nested loops

---
+++ python/13. nested loops/Main.py
@@ -0,0 +1,12 @@
+# nested loops = the "inner loop" will finish all of it's iterations before
+#               finishing one iteration of the "outer  loop"
+
+
+rows = int(input("cate randuri: "))
+columns = int(input("cate coloane: "))
+symbol = input("introduceti un simbol: ")
+
+for i in range(rows):
+    for j in range(columns):
+        print(symbol, end = "")
+    print()
