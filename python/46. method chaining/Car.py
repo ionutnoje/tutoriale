8a44f2d6b416e35c40acc0821f8b8b78a9e5aad7
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2021-12-19 19:48:50 +0200

    method overriding

---
+++ python/46. method chaining/Car.py
@@ -0,0 +1,17 @@
+class Car:
+
+    def turn_on(self):
+        print("you start the engine")
+        return self
+
+    def drive(self):
+        print("you drive the car")
+        return self
+
+    def brake(self):
+        print("you step on the brakes")
+        return self
+
+    def turn_off(self):
+        print("you turn off the engine")
+        return self
