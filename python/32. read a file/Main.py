92f748607942e2f85af39f7490b7738a9a27085e
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2021-12-19 12:46:45 +0200

    read from a file

--- python/32. read a file/Main.py
+++ python/32. read a file/Main.py
@@ -1,10 +1,25 @@
 
 #folosind with open o sa se inchida automat fisierul dupa citire
 
+#######################################################################
 try:
-    with open("test.txt") as file:
+    with open("test.txt","r") as file:
         print(file.read())
 except FileNotFoundError:
     print("that file was not found")
 
 print(file.closed) # o sa returneze true daca fisierul este inchis
+
+#######################################################################
+
+# Python program to read
+# file word by word
+
+# opening the text file
+with open("test.txt","r") as file:
+    # reading each line
+    for line in file:
+        # reading each word
+        for word in line.split():
+            # displaying the words
+            print(word)
