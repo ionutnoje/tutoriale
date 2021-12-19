a5007b7ad13f1c7bef0053829e72f39d89d7d245
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2021-12-19 19:16:01 +0200

    inheritance

---
+++ python/42. inheritance/Animals.py
@@ -0,0 +1,8 @@
+class Animals:
+    alive = True
+
+    def eat(self):
+        print("this animal is eating")
+
+    def sleep(self):
+        print("this animal is sleeping")
---
+++ python/42. inheritance/Fish.py
@@ -0,0 +1,12 @@
+from Animals import Animals
+
+class Fish(Animals):
+
+    def eat(self):
+        print("pestele mananca")
+
+    def sleep(self):
+        print("pestele doarme")
+
+    def swim(self):
+        print("pestele inoata")
