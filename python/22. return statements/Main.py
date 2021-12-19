7a2270a03be83fe81b15c7c612bf616e9742b4ef
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2021-12-19 11:09:56 +0200

    return statements

---
+++ python/22. return statements/Main.py
@@ -0,0 +1,11 @@
+# return statements = functions send python values/objects back to the caller.
+#                     these values/objects are known as the function's return value
+
+
+def multiply(number1, number2):
+    result = number1 * number2
+    return result
+
+valoare = multiply(2,3)
+
+print(valoare)
