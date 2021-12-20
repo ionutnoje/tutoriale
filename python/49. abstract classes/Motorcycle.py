61eb3388784e245259f4dd6c7c8660df1173d262
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2021-12-20 16:24:49 +0200

    abstract classes

---
+++ python/49. abstract classes/Motorcycle.py
@@ -0,0 +1,9 @@
+from Vehicle import Vehicle
+
+class Motorcycle(Vehicle):
+
+    def go(self):
+        print("you ride the motorcycle")
+
+    def stop(self):
+        print("this motorcycle is stopped")
