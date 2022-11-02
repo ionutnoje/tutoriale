ae4fb637eeb66937193148fb4d72bb2c13511f7e
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2022-11-02 21:51:53 +0200

    commit: introducere in numpy         python lists vs numpy array

---
+++ Numpy/1. Introduction/Introduction.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#the fundamental library needed for scientific computing with python is called numpy
+#numpy is a python library for array oriented computing
+#this open sorce library contiains:
+                    #a powerful n dimensional array object
+                    #advanced array silcing mehods
+                    #convenient array reshaping methods
+
+
+
+#numpy is good for:
+        #image processing
+        #signal processing
+        #linear algebra
+        #etc.
+
+#munpy does numerical computations veri fast....for 1000x1000 matrix in python takes > 10 minutes
+                                                                    #with numpy 0.03 secondsS
---
+++ Numpy/2. Python list vs numpy array.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+python_list = [1,2,3,4]
+numpy_array = np.array(python_list) # putem sa adaugam asa sau
+numpy_array2 = np.array([1,2,3,4,5,6,7])
+
+for i in python_list:
+    print(i) #printam toate elementele din lista python
+print("\n")
+
+for i in numpy_array:
+    print(i) # printam toate elementele din arrayul numpy
+print("\n")
+
+for i in numpy_array2:
+    print(i) #printam toate elementele din arrayul numpy2
+print("\n")
+
+
+#adaugare de element in lista python_list prin concatenare
+python_list = python_list + [10] # se adauga prin concatenare
+
+for i in python_list:
+    print(i)
+print("\n")
+
+#adaugare de elemente in lista python_list prin metoda .append()
+python_list.append(100)
+
+for i in python_list:
+    print(i)
+print("\n")
+
+
+#adaugare element in array ul numpy prin concatenare nu se poate
+numpy_array = numpy_array + [10000] #aceasta operatie o sa adauge 10000 la fiecare element existent in array
+
+for i in numpy_array:
+    print(i)
+print("\n")
+
+#adaugare element in array ul numpy prin .append() nu o sa mearga pentru ca nu are aceasta metoda
+
+
+#adunarea arrayurilor de tip numpy
+numpy_array = numpy_array + numpy_array
+
+for i in numpy_array:
+    print(i)
+print("\n")
+
+
+#adunarea listelor din python
+python_list = python_list + python_list # nu o sa se adune valorile celor 2 liste ci o sa se concateneze
+
+for i in python_list:
+    print(i)
+print("\n")
