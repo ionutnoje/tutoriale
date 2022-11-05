cdd1d27c3aa4e476f6611a25ac3918b1c547b30c
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2022-11-05 18:57:25 +0200

    mnist problem

--- TensorFlow/3. Neural Networks with Sequential and Functional API/Main.py
+++ TensorFlow/3. Neural Networks with Sequential and Functional API/Main.py
@@ -1,45 +1,59 @@
 import os
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras import layers
 from tensorflow.keras.datasets import mnist
-import pydot
-import graphviz
 
-# loading the dataset
+physical_devices = tf.config.list_physical_devices("GPU")
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-# print(x_test.shape)  # (10000, 28, 28)   10000 de poze fiecare sunte de 28 de pixeli pe 28 de pixeli
-# print(y_test.shape)
-
-# x_train = x_train.reshape(-1, 28 * 28)# -1 ca sa pastreze valorile asa cum sunt si sa le mutam pe un vector de 28 pe 28 (input layer)
-# conversia o sa se faci si o sa avem vectori numpy cu valori de tip float64 si noi avem nevoie de float32
-# deci o sa setam noi formatul si trebuie si normalizate, deoarece noi avem acum valori intre 0 si 255 si vrem intre 0 si 1
-
-x_train = x_train.reshape(-1, 28 * 28).astype("float32") / 255.0  # impartire pentru normalizare
+x_train = x_train.reshape(-1, 28 * 28).astype("float32") / 255.0 #transformam inputurile intr un numpy vector de 784 spatii
 x_test = x_test.reshape(-1, 28 * 28).astype("float32") / 255.0
 
-# cream modelul Sequential API din keras (very convenient, nor very flexible) one input mapped to one output
-model = keras.Sequential(
-    [  # aici punem layer urile
+## Sequential API (Very convenient, not very flexible)
+#model = keras.Sequential(
+#    [
+#        keras.Input(shape=(28 * 28)),
+#        layers.Dense(512, activation="relu"),
+#        layers.Dense(256, activation="relu"),
+#        layers.Dense(10, activation="softmax"),
+#    ]
+#)
 
-        layers.Dense(512, activation='relu'),  # layer fully connected hidden
-        layers.Dense(256, activation='relu'),  # hidden layer fully connected
-        layers.Dense(10)  # output layer
-    ]
+model = keras.Sequential()
+model.add(keras.Input(shape=(784), name="input_layer"))
+model.add(layers.Dense(512, activation="relu", name="hidden1"))
+model.add(layers.Dense(256, activation="relu", name="hidden2"))
+model.add(layers.Dense(10,activation="softmax", name="output_layer")) # daca nu avem activare aici trebuie ca la model.compile la loss...from_ligits sa fie true
+
+## Functional API (A bit more flexible)
+#inputs = keras.Input(shape=(784))
+#x = layers.Dense(512, activation="relu", name="first_layer")(inputs)
+#x = layers.Dense(256, activation="relu", name="second_layer")(x)
+#outputs = layers.Dense(10, activation="softmax")(x)
+#model = keras.Model(inputs=inputs, outputs=outputs)
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False), # from logits false este pentru ca noi avem outputul nenormalizat
+    optimizer=keras.optimizers.Adam(learning_rate=0.001),#adam este gradientul descendent
+    metrics=["accuracy"],
 )
 
-model.compile(  # aici ii spunem la keras cum sa configureze partea de training
-    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2) # aici pornim rularea
+model.evaluate(x_test, y_test, batch_size=32, verbose=2) # aici trecem la datele din testare
 
-    optimizer=keras.optimizers.Adam(learning_rate=0.01),
-    metrics=['accuracy']
-)
+print(model.summary()) #putem sa printam numai dupa model.fit sau daca folosim adaugarea layerelor cu add atunci se poate si mai sus
 
-model.fit(x_train,y_train, batch_size= 32, epochs=5, verbose=2)
-model.evaluate(x_test,y_test, batch_size= 32, verbose=2)
 
-model.summary()
+
+
+
+
+
+
+
+
