beae1752736167b5c4efb588d53b12d9eb0c3ce4
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2022-11-09 19:44:36 +0200

    ZTM: difference between changeable tensors and unchangeable tensors + some explanations

--- ZTM TensorFlow Course/2. Creating tensors with TensorFlow and tf.Variable()/Main.py
+++ ZTM TensorFlow Course/2. Creating tensors with TensorFlow and tf.Variable()/Main.py
@@ -9,7 +9,6 @@ variables: can be changed, like parameters, weight, bias etc.
 constant: can not be updated, but can be called multible times with only 1 copy in the memory
  """
 
-
 """Creating Tensors with tf.Variable() You can also (although you likely rarely will, because often, when working 
 with data, tensors are created for you automatically) create tensors using tf.Variable(). 
 
@@ -17,9 +16,7 @@ The difference between tf.Variable() and tf.constant() is tensors created with t
 changed,can only be used to create a new tensor), where as, tensors created with tf.Variable() are mutable (can be changed).
 """
 
-
 import tensorflow as tf
-
 
 # Create the same tensor with tf.Variable() and tf.constant()
 changeable_tensor = tf.Variable([10, 7])
@@ -29,10 +26,35 @@ print(unchangeable_tensor)
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-#Now let's try to change one of the elements of the changeable tensor.
+# Now let's try to change one of the elements of the changeable tensor.
 
 # Will error (requires the .assign() method)
+"""
 changeable_tensor[0] = 7
+print(changeable_tensor)
+"""
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# To change an element of a tf.Variable() tensor requires the assign() method.
+
+# Won't error
+changeable_tensor[0].assign(7)
 print(changeable_tensor)
 
 # ----------------------------------------------------------------------------------------------------------------------
+
+# Now let's try to change a value in a tf.constant() tensor.
+
+# Will error (can't change tf.constant())
+"""
+unchangeable_tensor[0].assign(7)
+print(unchangeable_tensor)
+"""
+"""
+we can't change the value of a constant tensor(created with tf.constant, but we can change the value of a changeable 
+tensor(created with tf.variable) the same way we would acces a value of an array + assign function
+"""
+
+
+#C:\Users\noje\Desktop\cursuri personale\inteligenta artificiala\[FreeCourseSite.com] Udemy - TensorFlow Developer Certificate in 2021 Zero to Mastery\2. Deep Learning and TensorFlow Fundamentals\12. Creating random tensors with TensorFlow.mp4
